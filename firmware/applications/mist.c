51e4f5956a872e5972ec8bccd2008c12b1b226cb
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-12-15 04:17:16 +0100

    a first application for testing: applications/mist.c An application replaces the entire firmware. It is not a l0dable.

--- firmware/applications/mist.c
+++ firmware/applications/mist.c
@@ -1,8 +1,10 @@
 #include <sysinit.h>
 #include "basic/basic.h"
 #include "lcd/render.h"
-void main_hello(void) {
-    DoString(10,20,"Ober-Doppel-Mist!");
-    lcdDisplay();
-    while (1) {}
+void main_mist(void) {
+  int y= 20;
+  DoString(0,y,"Ober-Doppel-");
+  DoString(4,y+getFontHeight()+4,"MIST!!!!");
+  lcdDisplay();
+  while (1) {}
 }
