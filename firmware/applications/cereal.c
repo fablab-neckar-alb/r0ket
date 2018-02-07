b3674bbb203928eaa370935945931b778665ff5e
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-07 01:53:33 +0100

    cereal.c : get LEDs to blink

--- firmware/applications/cereal.c
+++ firmware/applications/cereal.c
@@ -8,5 +8,13 @@ void main_cereal(void) {
   DoString(0,y,"Ur-Huara-");
   DoString(0,y+getFontHeight()+4,"Schiissdraeck!");
   lcdDisplay();
-  while (1) {}
+  char c= 0;
+  while (1) {
+    gpioSetValue(RB_LED0, c&1);
+    gpioSetValue(RB_LED1, (c>>1)&1);
+    gpioSetValue(RB_LED2, (c>>2)&1);
+    // for some reason, RB_LED3 seems to be unusable
+    delayms(200);
+    c++;
+  }
 }
