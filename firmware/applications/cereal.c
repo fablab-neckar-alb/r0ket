df379185c974f2a6417744aab6476beaf63a08df
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-07 03:58:13 +0100

    cereal.c : read in HB0..2 to LED0..2 LED 2 seems stuck LOW

--- firmware/applications/cereal.c
+++ firmware/applications/cereal.c
@@ -8,13 +8,15 @@ void main_cereal(void) {
   DoString(0,y,"Ur-Huara-");
   DoString(0,y+getFontHeight()+4,"Schiissdraeck!");
   lcdDisplay();
-  char c= 0;
+  gpioSetDir(RB_HB0, gpioDirection_Input);
+  gpioSetDir(RB_HB1, gpioDirection_Input);
+  gpioSetDir(RB_HB2, gpioDirection_Input);
+  //gpioSetDir(RB_HB3, gpioDirection_Input);
+  //gpioSetDir(RB_HB4, gpioDirection_Input);
+  //gpioSetDir(RB_HB5, gpioDirection_Input);
   while (1) {
-    gpioSetValue(RB_LED0, c&1);
-    gpioSetValue(RB_LED1, (c>>1)&1);
-    gpioSetValue(RB_LED2, (c>>2)&1);
-    // for some reason, RB_LED3 seems to be unusable
-    delayms(200);
-    c++;
+    gpioSetValue(RB_LED0, gpioGetValue(RB_HB0));
+    gpioSetValue(RB_LED1, gpioGetValue(RB_HB1));
+    gpioSetValue(RB_LED2, gpioGetValue(RB_HB2));
   }
 }
