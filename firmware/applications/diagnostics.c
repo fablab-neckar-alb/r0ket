fc7c10f172c7b8be61a0beadc566af8d53a20585
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-08 22:05:27 +0100

    diagnostics.c : monitor 16 bit timer 0 (control and count)

--- firmware/applications/diagnostics.c
+++ firmware/applications/diagnostics.c
@@ -1,7 +1,6 @@
 #include <sysinit.h>
 #include "basic/basic.h"
 #include "lcd/render.h"
-void backlightInit(void);
 
 /*
  For handling GPIO IRQs, see l0dable/Geigerct.c 
@@ -11,16 +10,17 @@ But maybe let's try timer IRQs first ...
 void main_diagnostics(void)
 {
   backlightInit();
+  //TMR_TMR16B0TCR= TMR_TMR1BB0TCR_COUNTERENABLE_ENABLED;
   while (1)
   {
     lcdClear();
-    lcdPrintln("MEMREMAP");
-    lcdPrintln(IntToStr(SCB_MEMREMAP,8,F_HEX));
-    lcdPrintln("ISER0");
-    lcdPrintln(IntToStr(NVIC->ISER[0],8,F_HEX));
     lcdPrintln("ISER1");
     lcdPrintln(IntToStr(NVIC->ISER[1],8,F_HEX));
+    lcdPrintln("TMR16B0 ctrl");
+    lcdPrintln(IntToStr(TMR_TMR16B0TCR,8,F_HEX));
+    lcdPrintln("TMR16B0 count ");
+    lcdPrintln(IntToStr(TMR_TMR16B0TC,8,F_HEX));
     lcdRefresh();
-    delayms(200);
+    //delayms(200);
   }
 }
