c7e5931e9a976332b67f2b41b7a13c2cf6ddd9f3
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-08 22:51:20 +0100

    diagnostics.c : use and display prescaler for TMR16B0

--- firmware/applications/diagnostics.c
+++ firmware/applications/diagnostics.c
@@ -11,19 +11,21 @@ But maybe let's try timer IRQs first ...
 void main_diagnostics(void)
 {
   SCB_SYSAHBCLKCTRL|= SCB_SYSAHBCLKCTRL_CT16B0; 
+  TMR_TMR16B0PR= 0xFFFFFFFF;
   TMR_TMR16B0TCR= TMR_TMR16B0TCR_COUNTERENABLE_ENABLED;
   while (1)
   {
     lcdClear();
     lcdPrintln("ISER1");
     lcdPrintln(IntToStr(NVIC->ISER[1],8,F_HEX));
-    lcdPrintln("TMR16B0 ctrl");
+    lcdPrintln("TMR16B0");
+    lcdPrint("ctrl ");
     lcdPrintln(IntToStr(TMR_TMR16B0TCR,8,F_HEX));
-    lcdPrintln("TMR16B0 pre");
-    lcdPrintln(IntToStr(TMR_TMR16B0PR,8,F_HEX));
-    lcdPrint("PRE16B0 ");
+    lcdPrint("PR ");
+    lcdPrintln(IntToStr(TMR_TMR16B0PR,4,F_HEX));
+    lcdPrint("PC ");
     lcdPrintln(IntToStr(TMR_TMR16B0PC,4,F_HEX));
-    lcdPrint("TMR16B0 ");
+    lcdPrint("TC ");
     lcdPrintln(IntToStr(TMR_TMR16B0TC,4,F_HEX));
     lcdRefresh();
     //delayms(200);
