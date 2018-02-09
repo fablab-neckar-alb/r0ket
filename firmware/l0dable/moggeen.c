4a262a8ebeb8ded48b221cb7481ca9aca054e699
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-09 07:25:41 +0100

    moggeen.c : timer interrupt and output works as l0dable, too.

--- firmware/l0dable/moggeen.c
+++ firmware/l0dable/moggeen.c
@@ -1,12 +1,8 @@
 #include <sysinit.h>
-#include <string.h>
 #include <stdlib.h>
 
 #include "basic/basic.h"
 #include "basic/config.h"
-
-#include "lcd/render.h"
-#include "lcd/print.h"
 
 #include "usetable.h"
 
