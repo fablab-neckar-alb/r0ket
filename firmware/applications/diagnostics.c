eb65f4c96d289a191939e5443a3159bcf37deea6
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-09 01:14:30 +0100

    diagnostics.c : only toggle SS2 in interrupt handler, toggle LED with delayms(1000) exact frequency is 59.52kHz => fin=59.52MHz, there are hiccups

--- firmware/applications/diagnostics.c
+++ firmware/applications/diagnostics.c
@@ -10,6 +10,7 @@ But maybe let's try timer IRQs first ...
 */
 void main_diagnostics(void)
 {
+  static uint8_t blink= 0;
   gpioSetDir(RB_LED1, gpioDirection_Output);
   gpioSetDir(RB_SPI_SS2, gpioDirection_Output);
   SCB_SYSAHBCLKCTRL|= SCB_SYSAHBCLKCTRL_CT16B0;
@@ -18,21 +19,20 @@ void main_diagnostics(void)
   TMR_TMR16B0MCR= TMR_TMR16B0MCR_MR0_RESET_ENABLED | TMR_TMR16B0MCR_MR0_INT_ENABLED;
   TMR_TMR16B0TCR= TMR_TMR16B0TCR_COUNTERENABLE_ENABLED;
   NVIC_EnableIRQ(TIMER_16_0_IRQn);
+  lcdClear();
+  lcdPrintln("ISER1");
+  lcdPrintln(IntToStr(NVIC->ISER[1],8,F_HEX));
+  lcdPrintln("TMR16B0");
+  lcdPrint("MCR ");
+  lcdPrintln(IntToStr(TMR_TMR16B0MCR,8,F_HEX));
+  lcdPrint("PR ");
+  lcdPrintln(IntToStr(TMR_TMR16B0PR,4,F_HEX));
+  lcdRefresh();
   while (1)
   {
-    lcdClear();
-    lcdPrintln("ISER1");
-    lcdPrintln(IntToStr(NVIC->ISER[1],8,F_HEX));
-    lcdPrintln("TMR16B0");
-    lcdPrint("MCR ");
-    lcdPrintln(IntToStr(TMR_TMR16B0MCR,8,F_HEX));
-    lcdPrint("PR ");
-    lcdPrintln(IntToStr(TMR_TMR16B0PR,4,F_HEX));
-    lcdPrint("PC ");
-    lcdPrintln(IntToStr(TMR_TMR16B0PC,4,F_HEX));
-    lcdPrint("TC ");
-    lcdPrintln(IntToStr(TMR_TMR16B0TC,4,F_HEX));
-    lcdRefresh();
+    delayms(1000);
+    blink= ~blink;
+    gpioSetValue(RB_LED1, 1 & blink);
   }
 }
 
@@ -40,7 +40,6 @@ void TIMER16_0_IRQHandler(void)
 {
   static uint8_t blink= 0;
   blink= ~blink;
-  gpioSetValue(RB_LED1, 1 & blink);
   gpioSetValue(RB_SPI_SS2, 1 & blink);
   TMR_TMR16B0IR|= TMR_TMR16B0IR_MR0;
 }
