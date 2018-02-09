6f6f68d47611fe48affb3ac163d3ca37a44f8e37
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-09 03:52:22 +0100

    diagnostics.c : bare bones version of program, no display, only pulse SPI_SS2

--- firmware/applications/diagnostics.c
+++ firmware/applications/diagnostics.c
@@ -1,17 +1,12 @@
 #include <sysinit.h>
 #include "basic/basic.h"
-//#include "lcd/render.h"
-#include "lcd/print.h"
 
 /*
- For handling GPIO IRQs, see l0dable/Geigerct.c 
-
-But maybe let's try timer IRQs first ...
+  Let's try timer IRQs,
+  absolute bare bones version
 */
 void main_diagnostics(void)
 {
-  static uint8_t blink= 0;
-  gpioSetDir(RB_LED1, gpioDirection_Output);
   gpioSetDir(RB_SPI_SS2, gpioDirection_Output);
   SCB_SYSAHBCLKCTRL|= SCB_SYSAHBCLKCTRL_CT16B0;
   TMR_TMR16B0PR= 44;
@@ -19,21 +14,6 @@ void main_diagnostics(void)
   TMR_TMR16B0MCR= TMR_TMR16B0MCR_MR0_RESET_ENABLED | TMR_TMR16B0MCR_MR0_INT_ENABLED;
   TMR_TMR16B0TCR= TMR_TMR16B0TCR_COUNTERENABLE_ENABLED;
   NVIC_EnableIRQ(TIMER_16_0_IRQn);
-  lcdClear();
-  lcdPrintln("ISER1");
-  lcdPrintln(IntToStr(NVIC->ISER[1],8,F_HEX));
-  lcdPrintln("TMR16B0");
-  lcdPrint("MCR ");
-  lcdPrintln(IntToStr(TMR_TMR16B0MCR,8,F_HEX));
-  lcdPrint("PR ");
-  lcdPrintln(IntToStr(TMR_TMR16B0PR,4,F_HEX));
-  lcdRefresh();
-  while (1)
-  {
-    delayms(1000);
-    blink= ~blink;
-    gpioSetValue(RB_LED1, 1 & blink);
-  }
 }
 
 void TIMER16_0_IRQHandler(void)
